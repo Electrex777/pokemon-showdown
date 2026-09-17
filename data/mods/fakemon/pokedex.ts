c8d2795b48da5485e3065b906fa9760b523e5858
Electrex777 <carlosherrerarodriguez81@gmail.com>
2026-09-17 18:44:44 +0200

    Crear mods/fakemon/pokedex.ts

---
+++ data/mods/fakemon/pokedex.ts
@@ -0,0 +1,11 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+
+kylesatirus: {
+num: -1001
+name: Kylesatirus, 
+baseSpecies: "Gogoat"
+noShowdownData: true, 
+types: ["Grass"], 
+baseStats: {hp: 80, atk: 103, def: 100, spa: 57, spd: 100, spe: 95}, 
+abilities: {0: "Overgrow", 1: "___", H: "Intrepid Sword"}, 
+weightkg: 85.5, 
